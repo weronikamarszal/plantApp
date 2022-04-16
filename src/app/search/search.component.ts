cacab724a655017bb77a45cee1f9e6e30c90a4a8
weronikamarszal <wero.marszal@gmail.com>
2022-04-16 17:47:02 +0200

    a lots of components

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchComponent implements OnInit {
   plants: any = [
     {
-      image: 'fikus.jpeg'
+      image: 'fikus.jpeg',
+      name: 'fikus'
     },
     {
-      image: 'kaktus.jpeg'
+      image: 'kaktus.jpeg',
+      name: 'kaktus'
     },
     {
-      image: 'strelicja.jpeg'
+      image: 'strelicja.jpeg',
+      name: 'strelicja'
+    },
+    {
+      image: 'kaktus.jpeg',
+      name: 'kaktus'
+    },
+    {
+      image: 'strelicja.jpeg',
+      name: 'strelicja'
     },
   ];
 
